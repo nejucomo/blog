096c479c806f2a65cad9ae10c2ef4758bb10fa17
Nathan Wilcox <nejucomo@gmail.com>
2014-08-13 09:05:25 -0700

    Only install dependencies when creating a new virtualenv.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 REPOROOT="$(readlink -f "$(dirname "$0")/..")"
 VENV="${REPOROOT}/build/venv"
 
 if ! [ -d "$VENV" ]
 then
-    echo "Creating virtualenv: $VENV"
+    echo "=== Creating virtualenv: $VENV ==="
     virtualenv "$VENV"
+
+    echo '=== Activating virtualenv ==='
+    source "$VENV/bin/activate"
+
+    echo '=== Installing/updating dependencies ==='
+    "$VENV/bin/pip" install --requirement "$REPOROOT/config/pip-requirement.txt"
 fi
 
-echo 'Installing/updating dependencies.'
-"$VENV/bin/pip" install --requirement "$REPOROOT/config/pip-requirement.txt"
-
-echo 'Generating site.'
-"$VENV/bin/pelican" --help
+echo '=== Generating site ==='
+"$VENV/bin/pelican" \
+    --settings "$REPOROOT/config/pelicanconf.py" \
+    --output "$REPOROOT/build/output" \
+    --cache-path "$REPOROOT/build/cache" \
+    --delete-output-directory \
+    "$REPOROOT/content"
