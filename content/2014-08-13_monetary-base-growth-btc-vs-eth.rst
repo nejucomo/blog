3ae2b5eb238c71ad1c19bf643841e7dc2b9cdab2
Nathan Wilcox <nejucomo@gmail.com>
2014-08-14 08:30:35 -0700

    Add text describing the charts, add more charts.

--- content/2014-08-13_monetary-base-growth-btc-vs-eth.rst
+++ content/2014-08-13_monetary-base-growth-btc-vs-eth.rst
@@ -8,13 +8,104 @@
 	<script language="javascript" type="text/javascript" src="./js/flot-axislabels/jquery.flot.axislabels.js"></script>
 	<script language="javascript" type="text/javascript" src="./js/monetary-base-growth-btc-vs-eth.js"></script>
 
+
+Here are some plots for comparing the monetary policies of Bitcoin versus Ethereum.  I created the datasets for these from scratch, based on ``FIXME: add references``.  They are approximated to years after launch, whereas a block-count on the X-axes would be more precise.  For Ethereum, I use ``𝑥`` as the total number of `ETH` allocated in the genesis block.  ``FIXME: double check 𝑥 and reward rate are correct.``
+
+**Caveat Emptor:**  It's quite possible I've made mistakes or errors.  Do not rely on this data without careful verification on your own.
+
+Block Rewards
+=============
+
+In both systems, the change in total monetary base comes exclusively from "block rewards" where the miner which solves a constraint receives a given number of new currency units.  Bitcoin has a policy of halving the reward every four years until it reaches 0 due to rounding error.  ``FIXME: Correct?``  Ethereum has a policy of a fixed reward into perpetuity:
+
+.. raw:: html
+
     <div style="text-align: center">
       Mining Reward
       <div id="div-plot_monetary-base-btc-vs-eth-mining-reward" style="width: 100%; height: 200px"></div>
     </div>
 
+**Note:** These two data sets have distinct units, so the cross-over around year 16 is a plotting artifact without significance.  ``FIXME: What's a better term than "data set"?``
+
+Monetary Base
+=============
+
+I use the term `monetary base` to refer to the number of currency units allocated by the protocol.  Note that this is distinct from `money supply` in that the latter refers to the availability of funds for actors in the system, and that availability is affected by lost private keys, transfers to nonexistent addresses, low exchange liquidity, and other factors.
+
+Given that both systems have a monetary policy where the only change in the monetary base is by block rewards, then in Bitcoin the long term monetary base converges to a fixed amount, ``21,000,000 BTC``, whereas in Ethereum it grows perpetually.  Here's what this looks like for the first 24 years after launch:
+
+.. raw:: html
+
     <div style="text-align: center">
       Monetary Base
       <div id="div-plot_monetary-base-btc-vs-eth-monetary-base" style="width: 100%; height: 400px"></div>
     </div>
+
+**Note:** As for the rewards plot, the two data sets have distinct Y-axes and scaling, so there's nothing special about year 23.
+
+Growth Rate
+===========
+
+Here's where the comparison gets interesting.  If we divide the monetary base by the previous year, we get an annual monetary base growth rate.  Because this is a unitless percentage for both systems, we finally have a plot with a single Y-axis and now the crossover points *do* have significance.
+
+**Punchline:** Bitcoin's monetary base grows much faster than Ethereum's initially, approaches a similar level at four years after launch, and finally drops below Ethereum around year 8.  Forever after, Bitcoin has lower growth and reaches 0, whereas Ethereum's growth rate always remains positive (see next plot).
+
+.. raw:: html
+
+    <div style="text-align: center">
+      Growth Rate
+      <div id="div-plot_monetary-base-btc-vs-eth-growth-rate" style="width: 100%; height: 400px"></div>
+    </div>
+
+**Note:** Bitcoin starts at 0 BTC in its genesis block, so the percentage growth at year 1 is infinite and is excluded.  By contrast, Ethereum starts with an initial allocation of ``𝑥 ETH``.
+
+Long Term Growth Rate
+---------------------
+
+Here's the same comparison over 100 years, rather than 24.  By starting at year 4, we omit the early discrepency which keeps the scales of the two datasets closer.  This highlights their difference in the long run:
+
+.. raw:: html
+
+    <div style="text-align: center">
+      Growth Rate - Years 4 to 100
+      <div id="div-plot_monetary-base-btc-vs-eth-growth-rate-long-term" style="width: 100%; height: 400px"></div>
+    </div>
+
+Growth Ratio
+============
+
+If ``B(Y)`` is the annual growth rate of the Bitcoin monetary base ``Y`` years after launch, and ``E(Y)`` is the annual Ethereum growth rate, then what is their ratio, ``E(Y) / B(Y)``?
+
+.. raw:: html
+
+    <div style="text-align: center">
+      Growth Rate Ratio
+      <div id="div-plot_monetary-base-btc-vs-eth-growth-rate-ratio" style="width: 100%; height: 400px"></div>
+    </div>
+
+Here we again see that Ethereum grows slower than Bitcoin until the 8th year, and dramatically so before year 4.  Up until year 23, at least the growth rate of Ethereum is around 5% more than Bitcoin.  Because the Bitcoin growth rate reaches 0, we should expect this ratio to sky rocket in the long term.  Let's look at that:
+
+Long Term Growth Ratio
+----------------------
+
+The 4 to 100 year ratio of growth rates:
+
+
+.. raw:: html
+
+    <div style="text-align: center">
+      Growth Rate - Years 4 to 100
+      <div id="div-plot_monetary-base-btc-vs-eth-growth-rate-ratio-long-term" style="width: 100%; height: 400px"></div>
+    </div>
+
+Here's the same plot at log scale:
+
+.. raw:: html
+
+    <div style="text-align: center">
+      Growth Rate - Years 4 to 100 (Log Scale)
+      <div id="div-plot_monetary-base-btc-vs-eth-growth-rate-ratio-long-term-log-scale" style="width: 100%; height: 400px"></div>
+    </div>
+
+
 
--- content/js/monetary-base-growth-btc-vs-eth.js
+++ content/js/monetary-base-growth-btc-vs-eth.js
@@ -4,12 +4,26 @@ $(function () {
   console.log('monetary base growth plotter is alive.')
 
   var YEARS = 24
+  var YEARS_LONG = 140
   var BTC_BLOCKS_PER_YEAR = 52500
   var XETH_INITIAL = 1.0
   var XETH_PER_YEAR = 0.27
 
+  var btc_mbase = gen_dataset(
+    0, YEARS, 1,
+    function (year, lmb) {
+      return lmb + btc_reward(year) * BTC_BLOCKS_PER_YEAR
+    },
+      -btc_reward(0) * BTC_BLOCKS_PER_YEAR
+  )
+
   plot_mining_reward()
-  plot_monetary_base()
+  plot_monetary_base(btc_mbase)
+  plot_growth_rate(btc_mbase)
+  plot_growth_rate_long_term()
+  plot_growth_rate_ratio()
+  plot_growth_rate_ratio_long_term()
+  plot_growth_rate_ratio_long_term_log_scale()
 
   return
 
@@ -29,10 +43,10 @@ $(function () {
       ],
       {
         xaxisLabels: { show: true },
-        xaxes: [{
+        xaxis: {
           axisLabel: 'Year after launch',
           ticks: 12,
-        }],
+        },
         yaxes: [
           {
             min: 0.1,
@@ -55,19 +69,13 @@ $(function () {
 
   }
 
-  function plot_monetary_base() {
+  function plot_monetary_base(btc_mbase) {
     var plot = $.plot(
       '#div-plot_monetary-base-btc-vs-eth-monetary-base',
       [
         {
           label: 'BTC',
-          data: gen_dataset(
-            0, YEARS, 1,
-            function (year, lmb) {
-              return lmb + btc_reward(year) * BTC_BLOCKS_PER_YEAR
-            },
-            -btc_reward(0) * BTC_BLOCKS_PER_YEAR
-          ),
+          data: btc_mbase,
         },
         {
           label: '𝑥⋅ETH',
@@ -83,31 +91,239 @@ $(function () {
       ],
       {
         xaxisLabels: { show: true },
-        xaxes: [{
+        xaxis: {
           axisLabel: 'Year after launch',
           ticks: 12,
-        }],
+        },
         yaxes: [
           {
-            /*
-            min: 0.1,
-            ticks: [0.1, 1, 10, 100],
-            tickDecimals: 1,
-            transform: function (v) { return Math.log(v) },
-            */
             tickFormatter: units_formatter(' BTC'),
           },
           {
             min: 0,
-            /*
-            ticks: [0.27],
-            tickDecimals: 2,
-            */
             position: 'right',
             alignTicksWithAxis: 'right',
             tickFormatter: units_formatter('⋅𝑥 ETH'),
           }
         ],
+        lines: { show: true },
+        points: { show: true },
+      })
+  }
+
+  function plot_growth_rate(btc_mbase) {
+    var to_percentage_growth = function (v) { return (v-1) * 100 }
+    var btc_data = gen_dataset(2, YEARS, 1, function (y) {
+      return to_percentage_growth(btc_mbase[y][1] / btc_mbase[y-1][1])
+    })
+
+    var eth_data = gen_dataset(1, YEARS, 1, function (y) {
+      return to_percentage_growth(
+        (XETH_INITIAL + y * XETH_PER_YEAR) / (XETH_INITIAL + (y - 1) * XETH_PER_YEAR)
+      )
+    })
+
+    var plot = $.plot(
+      '#div-plot_monetary-base-btc-vs-eth-growth-rate',
+      [
+        {
+          label: 'BTC growth per year',
+          data: btc_data,
+        },
+        {
+          label: 'ETH growth per year',
+          data: eth_data,
+        },
+      ],
+      {
+        xaxisLabels: { show: true },
+        xaxis: {
+          axisLabel: 'Year after launch',
+          ticks: 12,
+        },
+        yaxis: {
+          tickFormatter: units_formatter('%'),
+        },
+        lines: { show: true },
+        points: { show: true },
+      })
+  }
+
+  function plot_growth_rate_long_term() {
+    var btc_mbase = gen_dataset(
+      0, YEARS_LONG, 1,
+      function (year, lmb) {
+        return lmb + btc_reward(year) * BTC_BLOCKS_PER_YEAR
+      },
+        -btc_reward(0) * BTC_BLOCKS_PER_YEAR
+    )
+
+    var to_percentage_growth = function (v) { return (v-1) * 100 }
+    var btc_data = gen_dataset(4, YEARS_LONG, 1, function (y) {
+      return to_percentage_growth(btc_mbase[y][1] / btc_mbase[y-1][1])
+    })
+
+    var eth_data = gen_dataset(4, YEARS_LONG, 1, function (y) {
+      return to_percentage_growth(
+        (XETH_INITIAL + y * XETH_PER_YEAR) / (XETH_INITIAL + (y - 1) * XETH_PER_YEAR)
+      )
+    })
+
+    var plot = $.plot(
+      '#div-plot_monetary-base-btc-vs-eth-growth-rate-long-term',
+      [
+        {
+          label: 'BTC growth per year',
+          data: btc_data,
+        },
+        {
+          label: 'ETH growth per year',
+          data: eth_data,
+        },
+      ],
+      {
+        xaxisLabels: { show: true },
+        xaxis: {
+          axisLabel: 'Year after launch',
+          ticks: 12,
+        },
+        yaxes: [
+          {
+            tickFormatter: units_formatter('%'),
+          },
+          {
+            position: 'right',
+            alignTicksWithAxis: 'right',
+            tickFormatter: units_formatter('ETH gr / BTC gr'),
+          },
+        ],
+        lines: { show: true },
+        points: { show: true },
+      })
+  }
+
+  function plot_growth_rate_ratio() {
+    var btc_mbase = gen_dataset(
+      0, YEARS, 1,
+      function (year, lmb) {
+        return lmb + btc_reward(year) * BTC_BLOCKS_PER_YEAR
+      },
+        -btc_reward(0) * BTC_BLOCKS_PER_YEAR
+    )
+
+    var btc_data = gen_dataset(2, YEARS, 1, function (y) {
+      return btc_mbase[y][1] / btc_mbase[y-1][1]
+    })
+
+    var eth_data = gen_dataset(1, YEARS, 1, function (y) {
+      return (XETH_INITIAL + y * XETH_PER_YEAR) / (XETH_INITIAL + (y - 1) * XETH_PER_YEAR)
+    })
+
+    var plot = $.plot(
+      '#div-plot_monetary-base-btc-vs-eth-growth-rate-ratio',
+      [
+        {
+          label: 'ETH growth rate / BTC growth rate',
+          data: gen_dataset(2, YEARS, 1, function (y) {
+            return eth_data[y-1][1] / btc_data[y-2][1]
+          }),
+        },
+      ],
+      {
+        xaxisLabels: { show: true },
+        xaxis: {
+          axisLabel: 'Year after launch',
+          ticks: 12,
+        },
+        yaxes: {
+          tickFormatter: units_formatter('E(Y) / B(Y)'),
+        },
+        lines: { show: true },
+        points: { show: true },
+      })
+  }
+
+  function plot_growth_rate_ratio_long_term() {
+    var btc_mbase = gen_dataset(
+      0, YEARS_LONG, 1,
+      function (year, lmb) {
+        return lmb + btc_reward(year) * BTC_BLOCKS_PER_YEAR
+      },
+        -btc_reward(0) * BTC_BLOCKS_PER_YEAR
+    )
+
+    var btc_data = gen_dataset(2, YEARS_LONG, 1, function (y) {
+      return btc_mbase[y][1] / btc_mbase[y-1][1] - 1.0
+    })
+
+    var eth_data = gen_dataset(1, YEARS_LONG, 1, function (y) {
+      return (XETH_INITIAL + y * XETH_PER_YEAR) / (XETH_INITIAL + (y - 1) * XETH_PER_YEAR) - 1.0
+    })
+
+    var plot = $.plot(
+      '#div-plot_monetary-base-btc-vs-eth-growth-rate-ratio-long-term',
+      [
+        {
+          label: 'ETH growth rate / BTC growth rate',
+          data: gen_dataset(4, YEARS_LONG, 1, function (y) {
+            return eth_data[y-1][1] / btc_data[y-2][1]
+          }),
+        },
+      ],
+      {
+        xaxisLabels: { show: true },
+        xaxis: {
+          axisLabel: 'Year after launch',
+          ticks: 12,
+        },
+        yaxes: {
+          tickFormatter: units_formatter('E(Y) / B(Y)'),
+        },
+        lines: { show: true },
+        points: { show: true },
+      })
+  }
+
+  function plot_growth_rate_ratio_long_term_log_scale() {
+    var btc_mbase = gen_dataset(
+      0, YEARS_LONG, 1,
+      function (year, lmb) {
+        return lmb + btc_reward(year) * BTC_BLOCKS_PER_YEAR
+      },
+        -btc_reward(0) * BTC_BLOCKS_PER_YEAR
+    )
+
+    var btc_data = gen_dataset(2, YEARS_LONG, 1, function (y) {
+      return btc_mbase[y][1] / btc_mbase[y-1][1] - 1.0
+    })
+
+    var eth_data = gen_dataset(1, YEARS_LONG, 1, function (y) {
+      return (XETH_INITIAL + y * XETH_PER_YEAR) / (XETH_INITIAL + (y - 1) * XETH_PER_YEAR) - 1.0
+    })
+
+    var plot = $.plot(
+      '#div-plot_monetary-base-btc-vs-eth-growth-rate-ratio-long-term-log-scale',
+      [
+        {
+          label: 'ETH growth rate / BTC growth rate',
+          data: gen_dataset(4, YEARS_LONG, 1, function (y) {
+            return eth_data[y-1][1] / btc_data[y-2][1]
+          }),
+        },
+      ],
+      {
+        xaxisLabels: { show: true },
+        xaxis: {
+          axisLabel: 'Year after launch',
+          ticks: 12,
+        },
+        yaxis: {
+          min: 0.5,
+          ticks: [1e0, 1e2, 1e4, 1e6, 1e8, 1e10],
+          tickDecimals: 1,
+          transform: function (v) { return Math.log(v) },
+          tickFormatter: units_formatter(' E(Y) / B(Y)'),
+        },
         lines: { show: true },
         points: { show: true },
       })
@@ -131,6 +347,7 @@ $(function () {
   }
 
   function btc_reward(year) {
-    return 50 / Math.pow(2, Math.floor(year/4))
+    var r = 50 / Math.pow(2, Math.floor(year/4))
+    return (r < 1e-8) ? 0 : r
   }
 })
